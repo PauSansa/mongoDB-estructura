f702ef6c7c4c32504725548efb846a899d16c5d1
Sansa <pausansa59@gmail.com>
2023-02-24 22:38:56 +0100

    Update creation

--- 01-cul_d_ampolla/creation.js
+++ 01-cul_d_ampolla/creation.js
@@ -1,6 +1,94 @@
-
-db.createCollection( 'ventas', {validator: {$jsonSchema: {bsonType: 'object',title:'ventas',properties: {fecha_venta: {bsonType: 'date'},empleado: {bsonType: 'object',
-title:'object',properties: {nombre: {bsonType: 'string'},apellido: {bsonType: 'string'}}},gafas: {bsonType: 'object',
-title:'object',properties: {proveedor: {bsonType: 'object',
-title:'object',properties: {nombre: {bsonType: 'string'},direccion_postal: {bsonType: 'string'},telefono: {bsonType: 'timestamp'},email: {bsonType: 'timestamp'}}},marca: {bsonType: 'string'},grad_left: {bsonType: 'decimal'},grad_right: {bsonType: 'decimal'},tipo_montura: {enum: },color_montura: {bsonType: 'string'},color_left: {bsonType: 'string'},color_right: {bsonType: 'string'},precio: {bsonType: 'decimal'}}},cliente: {bsonType: 'object',
-title:'object',properties: {nombre: {bsonType: 'string'},direccion_postal: {bsonType: 'string'},telefono: {bsonType: 'int'},email: {bsonType: 'string'},fecha_registro: {bsonType: 'date'}}}}         }      }});  
+db.createCollection('ventas', {
+    validator: {
+        $jsonSchema: {
+            bsonType: 'object',
+            title: 'ventas',
+            properties: {
+                fecha_venta: {
+                    bsonType: 'date'
+                },
+                empleado: {
+                    bsonType: 'object',
+                    title: 'object',
+                    properties: {
+                        nombre: {
+                            bsonType: 'string'
+                        },
+                        apellido: {
+                            bsonType: 'string'
+                        }
+                    }
+                },
+                gafas: {
+                    bsonType: 'object',
+                    title: 'object',
+                    properties: {
+                        proveedor: {
+                            bsonType: 'object',
+                            title: 'object',
+                            properties: {
+                                nombre: {
+                                    bsonType: 'string'
+                                },
+                                direccion_postal: {
+                                    bsonType: 'string'
+                                },
+                                telefono: {
+                                    bsonType: 'timestamp'
+                                },
+                                email: {
+                                    bsonType: 'timestamp'
+                                }
+                            }
+                        },
+                        marca: {
+                            bsonType: 'string'
+                        },
+                        grad_left: {
+                            bsonType: 'decimal'
+                        },
+                        grad_right: {
+                            bsonType: 'decimal'
+                        },
+                        tipo_montura: {
+                            enum: ["flotante", "pasta", "metalica"]
+                        },
+                        color_montura: {
+                            bsonType: 'string'
+                        },
+                        color_left: {
+                            bsonType: 'string'
+                        },
+                        color_right: {
+                            bsonType: 'string'
+                        },
+                        precio: {
+                            bsonType: 'decimal'
+                        }
+                    }
+                },
+                cliente: {
+                    bsonType: 'object',
+                    title: 'object',
+                    properties: {
+                        nombre: {
+                            bsonType: 'string'
+                        },
+                        direccion_postal: {
+                            bsonType: 'string'
+                        },
+                        telefono: {
+                            bsonType: 'int'
+                        },
+                        email: {
+                            bsonType: 'string'
+                        },
+                        fecha_registro: {
+                            bsonType: 'date'
+                        }
+                    }
+                }
+            }
+        }
+    }
+});
